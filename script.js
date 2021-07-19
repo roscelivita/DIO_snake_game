d408fba69486a7aa6466ff790a08ecb81015f28f
roscelivita <roscelivita@gmail.com>
2021-07-19 20:52:59 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let canvas = document.getElementById("snake")
 let contexto = canvas.getContext("2d") //bidimensional: eixo x e y
 let box = 32 //define o grid do canvas, cada quadrado com 32 pixels
+let snake = []
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
+/* criar o background */
 function criarBG(){
     contexto.fillStyle = "lightgreen" //cor do background do canvas - estilo do contexto
     contexto.fillRect(0, 0, 16 * box, 16 * box) //desenha o retângulo, onde o jogo irá acontecer
@@ -12,4 +18,19 @@ function criarBG(){
                 o mesmo vale para largura*/
 }
 
+/* a cobrinha será um array de coordenadas 
+    -> dinâmica: adicionar um elemento e retirar o último */
+function criarCobrinha(){
+    for(let i = 0; i < snake.length; i++){
+        contexto.fillStyle = "green";
+        contexto.fillRect(snake[i].x, snake[i].y, box, box)
+        console.log(snake[i].x, snake[i].y, box, box)
+    }
+}
+
 criarBG()
+criarCobrinha()
+console.log(snake[0].x, snake[0].y, box, box)
+
+
+
