640b2767cdcc040a950b3c0493cec66a54df5a8d
roscelivita <roscelivita@gmail.com>
2021-07-19 22:06:23 -0300

    game over quando a cobrinha choca consigo mesma

--- script.js
+++ script.js
@@ -60,6 +60,14 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box
 
+    /* controlar o choque da cabeça da cobra com o seu próprio corpo */
+    for(let i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo)
+            alert("Game over =(")
+        }
+    }
+
     criarBG()
     criarCobrinha()
     criarComida()
